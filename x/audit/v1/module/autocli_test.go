f7d6f4a85625addb4622b1362e495e9f306ff6fb
J Bilal rafique <113895287+j-rafique@users.noreply.github.com>
2026-04-13 16:50:24 +0500

    fix(supernode): repair get-metrics AutoCLI args (#114)
    * fix(supernode): repair get-metrics AutoCLI args
    
    * test(cli): harden autogenerated command coverage

---
+++ x/audit/v1/module/autocli_test.go
@@ -0,0 +1,19 @@
+package audit
+
+import (
+	"testing"
+
+	"github.com/LumeraProtocol/lumera/testutil/autoclitest"
+	"github.com/LumeraProtocol/lumera/x/audit/v1/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAutoCLIOptions_CoversAllRPCs(t *testing.T) {
+	opts := AppModule{}.AutoCLIOptions()
+	require.NotNil(t, opts)
+	require.NotNil(t, opts.Query)
+	require.NotNil(t, opts.Tx)
+
+	autoclitest.AssertServiceMethodsCovered(t, types.Query_serviceDesc, opts.Query.RpcCommandOptions)
+	autoclitest.AssertServiceMethodsCovered(t, types.Msg_serviceDesc, opts.Tx.RpcCommandOptions, "UpdateParams")
+}
